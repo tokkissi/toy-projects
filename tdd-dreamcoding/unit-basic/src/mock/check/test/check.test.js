6fc2f291bf19ea4b7a6b263ce3f7ab45bcbc03ae
tokkissi <alal4674@gmail.com>
2024-01-04 19:25:56 +0900

    [tdd-dreamcoding][test] 첫인자가 true, false 일때의 check 함수 테스트

--- tdd-dreamcoding/unit-basic/src/mock/check/test/check.test.js
+++ tdd-dreamcoding/unit-basic/src/mock/check/test/check.test.js
@@ -1,1 +1,25 @@
-const check = require('../check');
+const check = require("../check");
+
+describe("check", () => {
+  let onSuccess;
+  let onFail;
+
+  beforeEach(() => {
+    onSuccess = jest.fn();
+    onFail = jest.fn();
+  });
+
+  it("전달된 게 true이면", () => {
+    check(() => true, onSuccess, onFail);
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    expect(onFail).toHaveBeenCalledTimes(0);
+    expect(onSuccess).toHaveBeenCalledWith("yes");
+  });
+  it("전달된 게 false이면", () => {
+    check(() => false, onSuccess, onFail);
+    expect(onFail).toHaveBeenCalled();
+    expect(onFail).toHaveBeenCalledTimes(1);
+    expect(onSuccess).toHaveBeenCalledTimes(0);
+    expect(onFail).toHaveBeenCalledWith("no");
+  });
+});
